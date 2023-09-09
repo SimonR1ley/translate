c8d547d8d07463a5c552bf64a4625fcbc4b47176
SimonBRiley <43470764+SimonR1ley@users.noreply.github.com>
2023-09-09 16:18:19 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useState } from "react";
+
+import Axios from "axios";
 
 function App() {
+  const [leftText, setLeftTextCon] = useState("");
+
+  const [selectVal, setSelectVal] = useState("it");
+
+  const [leftTextDisplay, setLeftTextDisplayCon] = useState("");
+
+  const [translatedText, setTranslatedText] = useState("");
+
+  const handleSelectChange = (event) => {
+    // Update the state variable when the user selects an option
+    setSelectVal(event.target.value);
+  };
+
+  const translateText = async () => {
+    try {
+      const response = await Axios.get(
+        `https://api.mymemory.translated.net/get?q=${leftText}&langpair=en|${selectVal}`
+      );
+      setTranslatedText(response.data.matches[0].translation);
+
+      setLeftTextCon("");
+    } catch (error) {
+      console.error("Error translating text:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-con">
+        <h1>Translate</h1>
+        <div className="container">
+          <div className="left-side-con">
+            <input
+              value={leftText}
+              placeholder="Text in English"
+              onChange={(e) => {
+                setLeftTextCon(e.target.value);
+                setLeftTextDisplayCon(e.target.value);
+              }}
+              onBlur={translateText}
+            />
+            <div className="left-side-inner">
+              <text>{leftTextDisplay}</text>
+            </div>
+          </div>
+          <div className="right-side-con">
+            <select
+              name="language"
+              value={selectVal}
+              onChange={handleSelectChange}
+            >
+              <option value="it">Italian</option>
+              <option value="de">German</option>
+              <option value="es">Spanish</option>
+              <option value="el">Greek</option>
+            </select>
+            {/* <input
+              placeholder="English"
+              onChange={(e) => setRightTextCon(e.target.value)}
+            /> */}
+            <div className="right-side-inner">
+              <text>{translatedText}</text>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
